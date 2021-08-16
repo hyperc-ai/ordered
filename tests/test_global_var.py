788202c645a3f6f89c0f2a5705789dff40c4b466
Andrew Gree <andrew@hyperc.com>
2021-08-15 15:11:37 -1000

    added reproducer for global variable issue #2

---
+++ tests/test_global_var.py
@@ -0,0 +1,25 @@
+__author__ = "Andrew Gree"
+__copyright__ = "CriticalHop Inc."
+__license__ = "MIT"
+
+
+import ordered
+import pytest
+
+x = 0
+def inc():
+    global x
+    x += 1
+
+def run_inc():
+    global x
+    with ordered.orderedcontext():
+        while x != 1:
+            ordered.choice()()
+    pass  # required due to current Python limitation
+
+pytest.mark.skip("Fix requested at https://github.com/hyperc-ai/ordered/issues/2")
+def test_global_access():
+    "Setting the breakpoint to line 13 should work"
+    run_inc()
+
